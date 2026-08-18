e8930675747436b6f850dfce3a32d3b4d33e6cd3
Romshh <Romshh@users.noreply.github.com>
2026-08-18 16:34:35 +0300

    ILI9341 320x240 display library for STM32 over SPI

---
+++ Core/Src/main.c
@@ -0,0 +1,427 @@
+/* USER CODE BEGIN Header */
+/* https://github.com/Romshh */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+#include "main.h"
+
+/* USER CODE BEGIN Includes */
+#include "lcd.h"
+
+/* USER CODE END Includes */
+
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+SPI_HandleTypeDef hspi1;
+
+UART_HandleTypeDef huart3;
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+void SystemClock_Config(void);
+static void MPU_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_SPI1_Init(void);
+static void MX_USART3_UART_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* USER CODE BEGIN 0 */
+
+#define LOGO_SIZE   56
+#define LOGO_SCALE   3
+
+static const char *logo[LOGO_SIZE] = {
+  ".........................######.........................",
+  "....................################....................",
+  "..................####################..................",
+  "...............#########################................",
+  "..............############################..............",
+  "............################################............",
+  "...........##################################...........",
+  "..........####################################..........",
+  ".........######################################.........",
+  "........########################################........",
+  ".......##########################################.......",
+  "......############################################......",
+  ".....##########.########################.##########.....",
+  ".....#########.....##################.....#########.....",
+  "....##########.......##..........##.......##########....",
+  "....##########............................##########....",
+  "...###########............................###########...",
+  "...###########............................###########...",
+  "..############............................############..",
+  "..############............................############..",
+  "..############............................############..",
+  ".############..............................############.",
+  ".############..............................############.",
+  ".###########................................###########.",
+  ".###########................................###########.",
+  ".###########................................###########.",
+  "############................................############",
+  "############................................############",
+  "############................................############",
+  "############................................############",
+  "############................................############",
+  "############................................############",
+  ".############..............................############.",
+  ".############..............................############.",
+  ".#############............................#############.",
+  ".##############..........................##############.",
+  ".##############.........................###############.",
+  "..###############......................###############..",
+  "..################....................################..",
+  "...#####....########...............##################...",
+  "...#####.....##########..........####################...",
+  "...#######....#########..........####################...",
+  "....#######....#######............##################....",
+  ".....######.....####...............################.....",
+  ".....#######.......................################.....",
+  "......######.......................###############......",
+  ".......######......................##############.......",
+  "........######.....................##############.......",
+  "........########...................#############........",
+  "..........###########..............############.........",
+  "...........##########..............##########...........",
+  "............#########..............#########............",
+  "..............#######..............########.............",
+  "...............######..............######...............",
+  ".................###................###.................",
+  "........................................................",
+};
+
+/* USER CODE END 0 */
+
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  MPU_Config();
+
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  MX_GPIO_Init();
+  MX_SPI1_Init();
+  MX_USART3_UART_Init();
+  /* USER CODE BEGIN 2 */
+  lcd_init();
+
+  lcd_clear(BLACK);
+
+  uint16_t lx = (LCD_W - LOGO_SIZE * LOGO_SCALE) / 2;
+  uint16_t ly = 6;
+
+  for (uint16_t r = 0; r < LOGO_SIZE; r++) {
+    for (uint16_t c = 0; c < LOGO_SIZE; c++) {
+      if (logo[r][c] == '#') {
+        lcd_rect_fill(lx + c * LOGO_SCALE, ly + r * LOGO_SCALE,
+                      LOGO_SCALE, LOGO_SCALE, WHITE);
+      }
+    }
+  }
+
+  lcd_text((LCD_W - 6 * 6 * 4) / 2, 190, "Romshh", WHITE, BLACK, 4);
+
+  /* USER CODE END 2 */
+
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 96;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 2;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  if (HAL_PWREx_EnableOverDrive() != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+static void MX_SPI1_Init(void)
+{
+
+  /* USER CODE BEGIN SPI1_Init 0 */
+
+  /* USER CODE END SPI1_Init 0 */
+
+  /* USER CODE BEGIN SPI1_Init 1 */
+
+  /* USER CODE END SPI1_Init 1 */
+
+  hspi1.Instance = SPI1;
+  hspi1.Init.Mode = SPI_MODE_MASTER;
+  hspi1.Init.Direction = SPI_DIRECTION_2LINES;
+  hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
+  hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi1.Init.NSS = SPI_NSS_SOFT;
+  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_32;
+  hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi1.Init.CRCPolynomial = 7;
+  hspi1.Init.CRCLength = SPI_CRC_LENGTH_DATASIZE;
+  hspi1.Init.NSSPMode = SPI_NSS_PULSE_ENABLE;
+  if (HAL_SPI_Init(&hspi1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN SPI1_Init 2 */
+
+  /* USER CODE END SPI1_Init 2 */
+
+}
+
+static void MX_USART3_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART3_Init 0 */
+
+  /* USER CODE END USART3_Init 0 */
+
+  /* USER CODE BEGIN USART3_Init 1 */
+
+  /* USER CODE END USART3_Init 1 */
+  huart3.Instance = USART3;
+  huart3.Init.BaudRate = 9600;
+  huart3.Init.WordLength = UART_WORDLENGTH_8B;
+  huart3.Init.StopBits = UART_STOPBITS_1;
+  huart3.Init.Parity = UART_PARITY_NONE;
+  huart3.Init.Mode = UART_MODE_TX_RX;
+  huart3.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart3.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart3.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart3.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART3_Init 2 */
+
+  /* USER CODE END USART3_Init 2 */
+
+}
+
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOG_CLK_ENABLE();
+
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET);
+
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_RESET);
+
+  HAL_GPIO_WritePin(GPIOB, LD1_Pin|LD3_Pin|LD2_Pin, GPIO_PIN_RESET);
+
+  HAL_GPIO_WritePin(USB_PowerSwitchOn_GPIO_Port, USB_PowerSwitchOn_Pin, GPIO_PIN_RESET);
+
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_SET);
+
+  GPIO_InitStruct.Pin = USER_Btn_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(USER_Btn_GPIO_Port, &GPIO_InitStruct);
+
+  GPIO_InitStruct.Pin = RMII_MDC_Pin|RMII_RXD0_Pin|RMII_RXD1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  GPIO_InitStruct.Alternate = GPIO_AF11_ETH;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  GPIO_InitStruct.Pin = RMII_REF_CLK_Pin|RMII_MDIO_Pin|RMII_CRS_DV_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  GPIO_InitStruct.Alternate = GPIO_AF11_ETH;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  GPIO_InitStruct.Pin = GPIO_PIN_4|GPIO_PIN_6;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  GPIO_InitStruct.Pin = LD1_Pin|LD3_Pin|GPIO_PIN_6|LD2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  GPIO_InitStruct.Pin = RMII_TXD1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  GPIO_InitStruct.Alternate = GPIO_AF11_ETH;
+  HAL_GPIO_Init(RMII_TXD1_GPIO_Port, &GPIO_InitStruct);
+
+  GPIO_InitStruct.Pin = USB_PowerSwitchOn_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(USB_PowerSwitchOn_GPIO_Port, &GPIO_InitStruct);
+
+  GPIO_InitStruct.Pin = USB_OverCurrent_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(USB_OverCurrent_GPIO_Port, &GPIO_InitStruct);
+
+  GPIO_InitStruct.Pin = USB_SOF_Pin|USB_ID_Pin|USB_DM_Pin|USB_DP_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  GPIO_InitStruct.Alternate = GPIO_AF10_OTG_FS;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  GPIO_InitStruct.Pin = USB_VBUS_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(USB_VBUS_GPIO_Port, &GPIO_InitStruct);
+
+  GPIO_InitStruct.Pin = RMII_TX_EN_Pin|RMII_TXD0_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  GPIO_InitStruct.Alternate = GPIO_AF11_ETH;
+  HAL_GPIO_Init(GPIOG, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+void MPU_Config(void)
+{
+  MPU_Region_InitTypeDef MPU_InitStruct = {0};
+
+  HAL_MPU_Disable();
+
+  MPU_InitStruct.Enable = MPU_REGION_ENABLE;
+  MPU_InitStruct.Number = MPU_REGION_NUMBER0;
+  MPU_InitStruct.BaseAddress = 0x0;
+  MPU_InitStruct.Size = MPU_REGION_SIZE_4GB;
+  MPU_InitStruct.SubRegionDisable = 0x87;
+  MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL0;
+  MPU_InitStruct.AccessPermission = MPU_REGION_NO_ACCESS;
+  MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_DISABLE;
+  MPU_InitStruct.IsShareable = MPU_ACCESS_SHAREABLE;
+  MPU_InitStruct.IsCacheable = MPU_ACCESS_NOT_CACHEABLE;
+  MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
+
+  HAL_MPU_ConfigRegion(&MPU_InitStruct);
+
+  HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
+
+}
+
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+
+  /* USER CODE END 6 */
+}
+#endif
---
+++ README.md
@@ -0,0 +1,146 @@
+# stm32-ili9341-spi
+
+A small ILI9341 display driver for STM32, written with the HAL. Four files,
+no dependencies, no malloc.
+
+I wrote this while learning SPI on an STM32F7. It does drawing, images and
+scalable text, and nothing else. If you need touch or SD, this is not it.
+
+The repo is a complete STM32CubeIDE project, so you can just import it and
+run it to see if your wiring works. The demo draws the GitHub logo and my
+username under it.
+
+![demo output](docs/screen.png)
+
+This picture is not a photo. I generated it from the same logo array and the
+same font table the firmware uses, so it matches the panel pixel for pixel.
+
+## What I tested it on
+
+- NUCLEO-F767ZI (STM32F767ZIT6U)
+- 2.8" ILI9341 module, 240x320, the cheap red one with the SD slot
+- 3.3V, SPI1 at 3 MBit/s
+- STM32CubeIDE 2.2.0, CubeMX 6.18.1, arm-none-eabi-gcc 14.3
+
+Other STM32 families should be fine too. The driver only calls HAL_Delay,
+HAL_GPIO_WritePin and HAL_SPI_Transmit, and those are the same everywhere,
+so you only need to change the pins and the SPI handle in lcd_conf.h. I have
+not tried it on anything other than the F767 though.
+
+## Wiring
+
+| Display | Nucleo |
+| --- | --- |
+| SCK | PA5 |
+| MOSI | PB5 |
+| CS | PA4 |
+| DC | PA6 |
+| RESET | PB6 |
+| VCC | +3V3 |
+| LED | VDD |
+| GND | GND |
+
+Three things that cost me time here:
+
+- The module has no regulator. 3.3V only, do not put 5V on VCC.
+- Do not tie RESET to the supply. lcd_init() pulses it, and the display will
+  not wake up without that pulse.
+- If you forget the LED pin the backlight stays off and you see nothing, even
+  though the code is running fine.
+
+MOSI is on PB5 and not PA7 because PA7 is used by the Ethernet PHY on this
+board. If you use a different board, check that first.
+
+## CubeMX settings
+
+SPI1 as Transmit Only Master, hardware NSS disabled.
+
+| Setting | Value |
+| --- | --- |
+| Data Size | 8 Bits |
+| CPOL | Low |
+| CPHA | 1 Edge |
+| First Bit | MSB |
+| CRC | Disabled |
+| Prescaler | start around 3 MBit/s |
+
+CS, DC and RESET are plain GPIO outputs. Set CS and RESET to High as their
+initial state.
+
+Watch out for Data Size. If you never click that field CubeMX can leave it at
+4 bits, and then only the low half of every byte goes out. The display never
+wakes up and you get no error anywhere. I lost a good while on this, so check
+it first if you see a blank screen.
+
+## Using it in your own project
+
+Copy lcd.c into Core/Src, and lcd.h, lcd_conf.h and font5x7.h into Core/Inc.
+Then edit lcd_conf.h. That is the only file you should need to touch: SPI
+handle, the three pins, screen orientation and size, and the maximum text
+scale.
+
+```c
+#include "lcd.h"
+
+lcd_init();
+lcd_clear(BLACK);
+lcd_text(10, 10, "hello", WHITE, BLACK, 2);
+```
+
+Call lcd_init() after MX_SPI1_Init(), not before.
+
+## Functions
+
+```c
+void lcd_init(void);
+void lcd_clear(uint16_t color);
+void lcd_pixel(uint16_t x, uint16_t y, uint16_t color);
+void lcd_rect_fill(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t color);
+void lcd_rect(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t color);
+void lcd_image(uint16_t x, uint16_t y, uint16_t w, uint16_t h, const uint16_t *data);
+void lcd_char(uint16_t x, uint16_t y, char c, uint16_t fg, uint16_t bg, uint8_t scale);
+void lcd_text(uint16_t x, uint16_t y, const char *s, uint16_t fg, uint16_t bg, uint8_t scale);
+```
+
+Colors are RGB565. RED, GREEN, BLUE, BLACK and WHITE are in lcd.h, add your
+own if you need more.
+
+lcd_image() takes a flat array in row order, w*h entries, one uint16_t per
+pixel.
+
+The font is 5x7 in a 6x8 box, ASCII 32 to 126, so lowercase works. scale
+draws every font dot as a scale x scale square, so scale 3 gives you 18x24
+characters. Anything outside the table is drawn as a space.
+
+## Speed
+
+Blocking transfers, SPI at 3 MBit/s, HCLK 96 MHz:
+
+| What | Pixels | Time |
+| --- | --- | --- |
+| Full screen | 76800 | 516 ms |
+| Rectangle outline | 160 | 3 ms |
+
+## Limitations
+
+- Blocking only. No DMA version in here.
+- One font, one size family.
+- No reading back from the display, the SPI is transmit only.
+- No touch, no SD card.
+- Nothing is checked against the screen bounds. If you draw outside, the
+  window command goes out of range and the drawing quietly disappears.
+
+## On the actual board
+
+![the board running the demo](docs/example.jpeg)
+
+Same demo on my desk. The background looks dark blue instead of black
+because I use a TFT panel that is not capable of showing true black unlike OLED panels, the code really does
+send 0x0000.
+
+## License
+
+MIT. See LICENSE.
+
+The GitHub logo in the demo belongs to GitHub, I only used it as an example
+picture.
